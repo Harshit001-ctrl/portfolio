dfc7452205d9cf5a91dc9d5cc1fa83e7b86748e4
Harshit <harshitrao84@gmail.com>
2024-07-03 17:10:26 +0530

    work

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-VANTA.GLOBE({
+const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+    console.log(e)
+})
+
+function raf(time) {
+    lenis.raf(time)
+    requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
+
+VANTA.WAVES({
     el: "#globes",
     mouseControls: true,
     touchControls: true,
@@ -8,6 +21,7 @@ VANTA.GLOBE({
     scale: 1.00,
     scaleMobile: 1.00
 })
+
 
 
 var tl = gsap.timeline();
@@ -55,23 +69,96 @@ tl.to(".text-loader", {
 
     }, 'same')
 
-
-
-
-gsap.to(".main", {
+tl.to(".main", {
     opacity: 1,
-    backgroundColor:"white",
-     width:"100%",
+    width: "90%",
     scrollTrigger: {
         trigger: ".main",
-        markers: true,
-        scroller: "body",
-        start: "top 50%",
-        end: "top 30%",
+        // markers: true,
+        // scroller: "",
+        start: "top 60%",
+        end: "top 20%",
         scrub: 2,
-        yoyo:true,        
+        yoyo: true,
     }
 })
 
+gsap.to(".myskills", {
+   y:-40,
+    opacity: 1,
+    scrollTrigger: {
+        trigger: ".myskills",
+        // markers: true,
+        start: "50% 71%",
+        end: "50% 50%",
+        scrub: true,
+        duration: 0.9,
+    }
+},'sc')
+
+let tl2 = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".skills",
+        start: "top 68%",
+        end: "top 50%",
+        // markers: true,
+        scrub: true,
+    }
+},'sc');
+
+tl2.to("#mysql", {
+    opacity: 1,
+    filter: "blur(0px)",
+    delay: -0.2
+})
+
+tl2.to("#html", {
+    opacity: 1,
+    filter: "blur(0px)",
+    delay: -0.2
 
 
+})
+
+tl2.to("#php", {
+    opacity: 1,
+    filter: "blur(0px)",
+    delay: -0.2
+
+})
+tl2.to("#css", {
+    opacity: 1,
+    filter: "blur(0px)",
+    delay: -0.2
+
+})
+tl2.to("#javascript", {
+    opacity: 1,
+    filter: "blur(0px)",
+    delay: -0.2
+
+})
+tl2.to("#gsap", {
+    opacity: 1,
+    filter: "blur(0px)",
+    delay: -0.2
+
+})
+tl2.to("#github", {
+    opacity: 1,
+    filter: "blur(0px)",
+    delay: -0.2
+
+})
+tl2.to("#bootstrap", {
+    opacity: 1,
+    filter: "blur(0px)",
+    delay: -0.2
+
+})
+tl2.to("#react", {
+    opacity: 1,
+    filter: "blur(0px)",
+    delay: -0.2
+
+})
